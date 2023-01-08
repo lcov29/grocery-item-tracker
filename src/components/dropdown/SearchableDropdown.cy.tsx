6cfed25f9e3baa5dfc8004b30b5f643e060d72cb
Lucas Vossen <lv_code@outlook.com>
2023-01-08 23:33:46 +0100

    add component for searchable dropdown

---
+++ src/components/dropdown/SearchableDropdown.cy.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { SearchableDropdown } from './SearchableDropdown';
+
+
+describe('<SearchableDropdown />', () => {
+
+   const foodOptionList = ['Ananas', 'Apple', 'Pizza', 'Bread', 'Cornflakes', 'Banana'];
+
+
+   beforeEach(() => {
+      cy.mount(<SearchableDropdown id="foodItemDropdown" placeholderText="Food Item" optionList={foodOptionList} />);
+   });
+
+
+   it('renders', () => {});
+
+
+   it('displays user defined placeholder text', () => {
+      cy.get('#foodItemDropdown-input').invoke('attr', 'placeholder').should('eq', 'Food Item');
+   });
+
+
+   it('does not display placeholder text if not specified', () => {
+      cy.mount(<SearchableDropdown id="foodItemDropdown" optionList={['A']} />);
+      cy.get('#foodItemDropdown-input').invoke('attr', 'placeholder').should('eq', '');
+   });
+
+
+   it('displays user defined option list', () => {
+      cy.get('#foodItemDropdown-list option')
+         .should('have.length', foodOptionList.length)
+         .first()
+         .should('have.value', foodOptionList[0])
+         .next()
+         .should('have.value', foodOptionList[1])
+         .next()
+         .should('have.value', foodOptionList[2])
+         .next()
+         .should('have.value', foodOptionList[3])
+         .next()
+         .should('have.value', foodOptionList[4])
+         .next()
+         .should('have.value', foodOptionList[5]);
+   });
+
+
+   it('accepts user input matching at least one available option', () => {
+      cy.get('#foodItemDropdown-input')
+         .type('Cornflakes')
+         .blur()
+         .should('have.value', 'Cornflakes');
+   });
+
+
+   it('rejects user input that is not matching any available option', () => {
+      cy.get('#foodItemDropdown-input')
+         .type('CornflaKes')
+         .blur()
+         .should('have.value', '');
+   });
+
+});
---
+++ src/components/dropdown/SearchableDropdown.tsx
@@ -0,0 +1,38 @@
+import React, { ReactElement } from 'react';
+
+
+type SearchableDropdownProps = {
+   id: string;
+   placeholderText?: string;
+   optionList: string[];
+};
+
+
+function SearchableDropdown(props: SearchableDropdownProps): ReactElement {
+   const { id, placeholderText = '', optionList } = props;
+   const inputId = `${id}-input`;
+   const datalistId = `${id}-list`;
+
+
+   function handleUserInput(event: React.ChangeEvent<HTMLInputElement>) {
+      const { target } = event;
+      const isUserInputInOptionList = optionList.includes(target.value);
+      const isUserInputFilled = target.value !== '';
+
+      if (isUserInputFilled && !isUserInputInOptionList) {
+         target.value = '';
+      }
+   }
+
+
+   return (
+      <>
+         <input id={inputId} name={inputId} type="search" placeholder={placeholderText} list={datalistId} onBlur={handleUserInput} />
+         <datalist id={datalistId}>
+            {optionList.map((option) => <option key={option} value={option} />)}
+         </datalist>
+      </>
+   );
+}
+
+export { SearchableDropdown };
