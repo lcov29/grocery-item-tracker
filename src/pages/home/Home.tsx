7e5f31966e5b2f8520d6747db98fb2ebed3712de
Lucas Vossen <lv_code@outlook.com>
2023-01-11 22:50:28 +0100

    add page component for home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement } from 'react';
+import { MenuBar } from '../../components/menuBar/MenuBar';
+import { Category } from '../../components/category/Category';
+import { Table } from '../../components/table/Table';
+import './home-style.css';
+
+const menuEntryList = [
+   { entryText: 'Home' },
+   { entryText: 'Grocery Items' },
+   { entryText: 'Shopping List' },
+   { entryText: 'Reports' },
+   { entryText: 'Settings' }
+];
+const expirationHeaderList = ['Id', 'Product', 'Expiration Date'];
+const expirationRowList = [
+   ['12', 'Spaghetti', '03.02.2023'],
+   ['24', 'Mineral Water (6 x 1.5 liter)', '12.11.2025']
+];
+
+
+function Home(): ReactElement {
+   return (
+      <div id="homePageContainer">
+         <div id="menuBarContainer">
+            <MenuBar menuEntryList={menuEntryList} />
+         </div>
+         <div id="supplyOverviewContainer">
+            <h2>Supply Overview</h2>
+            <Category name="Food" isTopLevel />
+         </div>
+         <div id="upcomingExpirationDatesContainer">
+            <h2>Upcoming Expiration Dates</h2>
+            <Table headerList={expirationHeaderList} rowList={expirationRowList} />
+         </div>
+      </div>
+   );
+}
+
+
+export { Home };
