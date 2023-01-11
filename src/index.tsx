0897057e32e9e77d64439d5ee61ed8a91accf6b2
Lucas Vossen <lv_code@outlook.com>
2023-01-11 10:46:56 +0100

    update import statement

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ReactApp } from './ReactApp';
+import { ReactApp } from './ReactAppDemo';
 
 const container = document.getElementById('react-app');
 
