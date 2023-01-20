e04b7f6840bc3e76f794f7ae19b49a8e13b36fcd
Lucas Vossen <lv_code@outlook.com>
2023-01-20 13:36:33 +0100

    add test for module MenuEntry

---
+++ src/components/menu/MenuEntry/MenuEntry.cy.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { MenuEntry } from './MenuEntry';
+
+describe('<MenuEntry />', () => {
+
+
+   beforeEach(() => {
+
+      function addClassName(color: string): void {
+         const menuEntry = document.getElementById('menu-entry');
+         if (menuEntry) {
+            menuEntry.classList.add(color);
+         }
+      }
+
+      const menuEntry = (
+         <MenuEntry
+            id="menu-entry"
+            key={1}
+            button={{ text: 'Change Color', action: () => addClassName('red') }}
+            dropdown={(
+               <div>
+                  <button type="button" onClick={() => addClassName('blue')}>Blue</button>
+                  <button type="button" onClick={() => addClassName('green')}>Green</button>
+               </div>
+            )}
+         />
+      );
+
+      cy.viewport(1200, 600);
+      cy.mount(menuEntry);
+   });
+
+
+   it('renders', () => {});
+
+
+   it('renders with user defined id', () => {
+      cy.get('#menu-entry').should('exist');
+   });
+
+
+   it('renders user defined button text', () => {
+      cy.get('#menu-entry').should('contain.text', 'Color');
+   });
+
+
+   it('executes user defined click event', () => {
+      const menuEntry = cy.get('#menu-entry');
+      menuEntry.click();
+      menuEntry.should('have.class', 'red');
+   });
+
+
+   it('renders user defined dropdown', () => {
+      cy.get('#menu-entry').focus();
+
+      const dropdown = cy.get('.menu-entry-dropdown');
+      dropdown.should('contain.text', 'Blue');
+      dropdown.should('contain.text', 'Green');
+   });
+
+
+   it('renders dropdown icon for menu entries with dropdown', () => {
+      cy.get('#menu-entry').should('have.class', 'menu-entry-with-dropdown');
+   });
+
+
+   it('does not render dropdown icon for menu entries without dropdown', () => {
+      cy.mount(<MenuEntry id="menu-entry" button={{ text: 'Color' }} />);
+      cy.get('#menu-entry').should('not.have.class', 'menu-entry-with-dropdown');
+   });
+
+
+   it('displays user defined dropdown when mouse is over menu button in desktop view', () => {
+      cy.get('.menu-entry-dropdown').should('not.be.visible');
+      cy.get('#menu-entry').focus();
+      cy.get('.menu-entry-dropdown').should('be.visible');
+   });
+
+
+   it('displays user defined dropdown when menu button is clicked in mobile view', () => {
+      cy.viewport(650, 600);
+      cy.get('.menu-entry-dropdown').should('not.be.visible');
+
+      cy.get('#menu-entry').focus();
+      cy.get('.menu-entry-dropdown').should('not.be.visible');
+
+      cy.get('#menu-entry').click();
+      cy.get('.menu-entry-dropdown').should('be.visible');
+   });
+
+
+   it('hides dropdown when mouse is leaving displayed dropdown area in desktop view', () => {
+      cy.get('#menu-entry').trigger('mouseover');
+
+      const dropdown = cy.get('.menu-entry-dropdown');
+      dropdown.should('be.visible');
+      dropdown.trigger('mouseout');
+      dropdown.should('not.be.visible');
+   });
+
+
+   it('hides dropdown when mouse is leaving displayed dropdown area in mobile view', () => {
+      cy.viewport(650, 600);
+      cy.get('#menu-entry').click();
+
+      const dropdown = cy.get('.menu-entry-dropdown');
+      dropdown.should('be.visible');
+      dropdown.trigger('mouseout');
+      dropdown.should('not.be.visible');
+   });
+
+
+});
