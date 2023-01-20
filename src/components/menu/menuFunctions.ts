0e456c371a038ce46b2c78e14a8d3c6a9c77c45d
Lucas Vossen <lv_code@outlook.com>
2023-01-20 11:47:40 +0100

    remove commented out code

--- src/components/menu/menuFunctions.ts
+++ src/components/menu/menuFunctions.ts
@@ -3,12 +3,4 @@ function isDesktopView(windowInnerWidth: number): boolean {
 }
 
 
-/*
-function hideAllDropdowns(): void {
-   const dropdownList = document.querySelectorAll<HTMLElement>('.menu-entry-dropdown');
-   dropdownList.forEach((dropdown) => dropdown.classList.remove('visible'));
-}
-*/
-
-
 export { isDesktopView };
