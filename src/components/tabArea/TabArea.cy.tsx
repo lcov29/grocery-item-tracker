4f9c2fe4f64726582418ab056478c261e9800fc1
Lucas Vossen <lv_code@outlook.com>
2023-01-22 19:18:12 +0100

    add component tab area

---
+++ src/components/tabArea/TabArea.cy.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { TabArea } from './TabArea';
+
+
+describe('<TabArea />', () => {
+
+
+   beforeEach(() => {
+      const entryList = [
+         { headline: 'Option 1', content: <p>Option 1</p> },
+         { headline: 'Option 2', content: <p>Option 2</p> },
+         { headline: 'Option 3', content: <p>Option 3</p> }
+      ];
+      cy.mount(<TabArea entryList={entryList} />);
+   });
+
+
+   it('renders', () => {});
+
+
+   it('displays user defined tabs', () => {
+      const tabBar = cy.get('.tab-bar');
+      tabBar.should('contain.text', 'Option 1');
+      tabBar.should('contain.text', 'Option 2');
+      tabBar.should('contain.text', 'Option 3');
+   });
+
+
+   it('displays content of first entry by default', () => {
+      cy.get('.content-area').should('contain.text', 'Option 1');
+      cy.get('.tab-bar > :nth-child(1)').should('have.class', 'active-tab');
+      cy.get('.tab-bar > :nth-child(2)').should('not.have.class', 'active-tab');
+      cy.get('.tab-bar > :nth-child(3)').should('not.have.class', 'active-tab');
+   });
+
+
+   it('changes content upon clicking a tab button', () => {
+      cy.get('.tab-bar > :nth-child(2)').click();
+      cy.get('.content-area').should('contain.text', 'Option 2');
+      cy.get('.tab-bar > :nth-child(1)').should('not.have.class', 'active-tab');
+      cy.get('.tab-bar > :nth-child(2)').should('have.class', 'active-tab');
+      cy.get('.tab-bar > :nth-child(3)').should('not.have.class', 'active-tab');
+
+      cy.get('.tab-bar > :nth-child(3)').click();
+      cy.get('.content-area').should('contain.text', 'Option 3');
+      cy.get('.tab-bar > :nth-child(1)').should('not.have.class', 'active-tab');
+      cy.get('.tab-bar > :nth-child(2)').should('not.have.class', 'active-tab');
+      cy.get('.tab-bar > :nth-child(3)').should('have.class', 'active-tab');
+   });
+
+
+});
---
+++ src/components/tabArea/TabArea.tsx
@@ -0,0 +1,41 @@
+import React, { ReactElement, useState } from 'react';
+import './tabArea.css';
+
+
+type TabAreaProps = {
+   entryList: { headline: string, content: ReactElement }[]
+};
+
+
+function TabArea(props: TabAreaProps): ReactElement {
+   const { entryList } = props;
+   const [activeAreaIndex, setActiveAreaIndex] = useState(0);
+
+   function buildTabBar(): ReactElement {
+      return (
+         <div className="tab-bar">
+            { entryList.map((entry, index) => (
+               <button
+                  type="button"
+                  className={(index === activeAreaIndex) ? ' tab active-tab' : 'tab'}
+                  onClick={() => setActiveAreaIndex(index)}
+               >
+                  {entry.headline}
+               </button>
+            ))}
+         </div>
+      );
+   }
+
+   return (
+      <div className="tab-area">
+         { buildTabBar() }
+         <div className="content-area">
+            { entryList[activeAreaIndex].content }
+         </div>
+      </div>
+   );
+}
+
+
+export { TabArea };
