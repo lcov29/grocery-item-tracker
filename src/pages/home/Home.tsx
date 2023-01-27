a70b1f09c32ab641047134524a9e628a94f3dbe3
Lucas Vossen <lv_code@outlook.com>
2023-01-27 06:02:25 +0100

    update file

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -39,7 +39,29 @@ function Home(): ReactElement {
          </div>
          <div id="supplyOverviewContainer">
             <h2>Supply Overview</h2>
-            <Category name="Food" isTopLevel />
+            <Category
+               name="Food"
+               isTopLevel
+               contentList={[
+                  <Category
+                     name="Bread"
+                     additionalText="7 Items"
+                     contentList={[
+                        <Table
+                           headerList={['Id', 'Product', 'Amount']}
+                           rowList={[
+                              ['1', 'Toast', '4'],
+                              ['2', 'Baguette', '2']
+                           ]}
+                        />
+                     ]}
+                  />,
+                  <Category
+                     name="Noodles"
+                     additionalText="2 Items"
+                  />
+               ]}
+            />
          </div>
          <div id="upcomingExpirationDatesContainer">
             <h2>Upcoming Expiration Dates</h2>
