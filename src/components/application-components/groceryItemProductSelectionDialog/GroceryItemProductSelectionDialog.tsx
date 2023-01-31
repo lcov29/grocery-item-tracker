5cba1f89e3dc78668686628ec52bf9158afc612d
Lucas Vossen <lv_code@outlook.com>
2023-01-31 05:49:04 +0100

    add component GroceryItemProductSelectionDialog

---
+++ src/components/application-components/groceryItemProductSelectionDialog/GroceryItemProductSelectionDialog.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { ReactElement } from 'react';
+import { SearchableDropdown } from '../../base-components/searchableDropdown/SearchableDropdown';
+import { Counter } from '../../base-components/counter/Counter';
+import './groceryItemProductSelectionDialog.css';
+
+
+function GroceryItemProductSelectionDialog(): ReactElement {
+   return (
+      <div id="grocery-item-product-selection-container">
+         <h2>Select Grocery Item</h2>
+         <form id="product-selection-dialog" onSubmit={() => {}}>
+            <label htmlFor="input-product-name">Product Name </label>
+            <div id="product-name-line">
+               <SearchableDropdown
+                  id="input-product-name"
+                  optionList={[
+                     'Product 1',
+                     'Product 2',
+                     'Product 3'
+                  ]}
+               />
+               <button id="add-new-product-button" type="button" onClick={() => {}}>+</button>
+            </div>
+            <label htmlFor="input-category">Category</label>
+            <input id="input-category" type="text" disabled />
+            <label htmlFor="input-subcategory">Subcategory</label>
+            <input id="input-subcategory" type="text" disabled />
+            <label htmlFor="input-distributor">Distributor</label>
+            <input id="input-distributor" type="text" disabled />
+            <label htmlFor="input-amount">Amount</label>
+            <Counter value={3} suffix="Items" minimum={1} />
+            <label htmlFor="input-price-per-unit">Price Per Unit</label>
+            <input id="input-price-per-unit" type="number" />
+            <label htmlFor="input-expiration-date">Expiration Date</label>
+            <input type="date" id="input-expiration-date" />
+         </form>
+         <div id="add-button-container">
+            <button type="button" onClick={() => {}}>Add</button>
+         </div>
+      </div>
+   );
+
+}
+
+
+export { GroceryItemProductSelectionDialog };
