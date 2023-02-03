914d62e68b08a02449a54301067876fadf4b5486
Lucas Vossen <lv_code@outlook.com>
2023-02-03 12:34:56 +0100

    add use effect to fetch initial data

--- src/frontend/components/pages/home/Home.tsx
+++ src/frontend/components/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { Category } from '../../base-components/category/Category';
 import { Table } from '../../base-components/table/Table';
 import { Counter } from '../../base-components/counter/Counter';
@@ -13,10 +13,32 @@ const expirationRowList = [
 
 
 function Home(): ReactElement {
+
+   const [supplyOverviewData, setSupplyOverviewData] = useState({});
+   const [expriationData, setExpirationData] = useState({});
+
+
+   async function fetchData(route: string, handler: (value: JSON) => void) {
+      const response = await fetch(route);
+      if (response.ok) {
+         const data = await response.json();
+         handler(data);
+      }
+   }
+
+
+   useEffect(() => {
+      const pageId = window.location.hash.replace('#', '');
+      fetchData(`/${pageId}/data/supplyOverview`, setSupplyOverviewData);
+      fetchData(`/${pageId}/data/expirationDateOverview/5`, setExpirationData);
+   }, []);
+
+
    return (
       <>
          <div id="supplyOverviewContainer">
             <h2>Supply Overview</h2>
+            <h2>{ supplyOverviewData.response }</h2>
             <Category
                name="Food"
                isTopLevel
@@ -27,10 +49,10 @@ function Home(): ReactElement {
                      additionalText="7 Items"
                      contentList={[
                         <Table
-                           headerList={['Id', 'Product', 'Amount']}
+                           headerList={['Product', 'Amount']}
                            rowList={[
-                              ['1', 'Toast', '4'],
-                              ['2', 'Baguette', '2']
+                              ['Toast', '4'],
+                              ['Baguette', '2']
                            ]}
                         />
                      ]}
@@ -45,6 +67,7 @@ function Home(): ReactElement {
          </div>
          <div id="upcomingExpirationDatesContainer">
             <h2>Upcoming Expiration Dates</h2>
+            <h2>{ expriationData.response }</h2>
             <div className="counter-container">
                <Counter value={4} minimum={0} maximum={14} suffix="Days" />
             </div>
