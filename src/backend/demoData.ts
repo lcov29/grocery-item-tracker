6278750e469ebd45ace04c0b50db62b2be8faa69
Lucas Vossen <lv_code@outlook.com>
2023-02-03 12:33:48 +0100

    add routing for home data and add demo data

---
+++ src/backend/demoData.ts
@@ -0,0 +1,81 @@
+const homeSupplyOverviewData = {
+   categoryList: [
+      {
+         name: 'Food',
+         total: 34,
+         minimum: 23,
+         subCategoryList: [
+            {
+               name: 'Bread',
+               total: 5,
+               minimum: 4,
+               productList: [
+                  {
+                     name: 'Toast',
+                     total: 2,
+                     minimum: 3
+                  },
+                  {
+                     name: 'Baguette',
+                     total: 3,
+                     minimum: 4
+                  }
+               ]
+            },
+            {
+               name: 'Noodles',
+               total: 13,
+               minimum: 2,
+               productList: [
+                  {
+                     name: 'Spaghetti',
+                     total: 6
+                  },
+                  {
+                     name: 'Ravioli',
+                     total: 7
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         name: 'Beverages',
+         total: 5,
+         minimum: 4,
+         subCategoryList: [
+            {
+               name: 'Non Alcoholic Beverages',
+               total: 2,
+               productList: [
+                  {
+                     name: 'Mineral Water (6 x 1,5 Liter)',
+                     total: 1,
+                  },
+                  {
+                     name: 'Orange Juice',
+                     total: 1
+                  }
+               ]
+            },
+            {
+               name: 'Alcoholic Beverages',
+               total: 4,
+               productList: [
+                  {
+                     name: 'Beer',
+                     total: 2
+                  },
+                  {
+                     name: 'Red Wine',
+                     total: 2
+                  }
+               ]
+            }
+         ]
+      }
+   ]
+};
+
+
+export { homeSupplyOverviewData };
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { homeSupplyOverviewData } from './demoData';
 
 
 const app = express();
 const server = createServer(app);
 
 app.use('/', express.static('../dist/public'));
+
+app.use('/Home/data/supplyOverview', (request, response) => {
+   response.json(homeSupplyOverviewData);
+});
+
+app.use('/Home/data/expirationDateOverview/:dayLimit', (request, response) => {
+   response.json({ response: `Server: Expiration Date Overview (Limit: ${request.params.dayLimit})` });
+});
 
 const port = 8080;
 server.listen(port);
