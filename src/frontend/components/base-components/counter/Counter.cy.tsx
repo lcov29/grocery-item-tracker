477e030c79ca433e989e85e2024c25a826dc0f57
Lucas Vossen <lv_code@outlook.com>
2023-02-06 14:03:10 +0100

    update test file to use wrapper around component

--- src/frontend/components/base-components/counter/Counter.cy.tsx
+++ src/frontend/components/base-components/counter/Counter.cy.tsx
@@ -1,68 +1,80 @@
-/* eslint-disable react/jsx-no-bind */
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import { Counter } from './Counter';
+
+
+type CounterWrapperProps = {
+   defaultValue: number,
+   minimum?: number,
+   maximum?: number,
+   suffix?: string
+};
+
+
+function CounterWrapper(props: CounterWrapperProps): ReactElement {
+   const { defaultValue, minimum, maximum, suffix } = props;
+   const [value, setValue] = useState(defaultValue);
+
+   return (
+      <Counter
+         value={value}
+         setValue={setValue}
+         minimum={minimum}
+         maximum={maximum}
+         suffix={suffix}
+      />
+   );
+}
+
 
 describe('<Counter />', () => {
 
-   let counterValue = 5;
-
-   function setCounterValue(value: number) {
-      counterValue = value;
-   }
-
-
-   beforeEach(() => {
-      counterValue = 5;
-   });
-
 
    it('renders', () => {
-      cy.mount(
-         <Counter value={counterValue} setValue={setCounterValue} />
-      );
+      cy.mount(<CounterWrapper defaultValue={6} />);
    });
 
 
    it('displays user defined initial value', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} />);
+      cy.mount(<CounterWrapper defaultValue={5} />);
       cy.get('.counter-value').should('have.text', '5');
    });
 
 
    it('displays user defined initial value with user defined suffix', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} suffix="items" />);
+      cy.mount(<CounterWrapper defaultValue={5} suffix="items" />);
       cy.get('.counter-value').should('have.text', '5 items');
    });
 
 
    it('displays user defined minimum value when initial value is below that minimum', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} minimum={10} />);
+      cy.mount(<CounterWrapper defaultValue={5} minimum={10} />);
       cy.get('.counter-value').should('have.text', '10');
-      /*
-      cy.mount(<Counter value={-3} setValue={setCounterValue} minimum={0} />);
+
+      cy.mount(<CounterWrapper defaultValue={-3} minimum={0} />);
       cy.get('.counter-value').should('have.text', '0');
-      cy.mount(<Counter value={-7} setValue={setCounterValue} minimum={-3} />);
+
+      cy.mount(<CounterWrapper defaultValue={-7} minimum={-3} />);
       cy.get('.counter-value').should('have.text', '-3');
-      */
    });
 
 
    it('displays user defined maximum value when initial value is above that maximum', () => {
-      cy.mount(<Counter value={9} setValue={setCounterValue} maximum={5} />);
+      cy.mount(<CounterWrapper defaultValue={9} maximum={5} />);
       cy.get('.counter-value').should('have.text', '5');
-      cy.mount(<Counter value={9} setValue={setCounterValue} maximum={0} />);
+
+      cy.mount(<CounterWrapper defaultValue={9} maximum={0} />);
       cy.get('.counter-value').should('have.text', '0');
-      cy.mount(<Counter value={9} setValue={setCounterValue} maximum={-5} />);
+
+      cy.mount(<CounterWrapper defaultValue={9} maximum={-5} />);
       cy.get('.counter-value').should('have.text', '-5');
-      cy.mount(<Counter value={9} setValue={setCounterValue} maximum={-5} />);
-      cy.get('.counter-value').should('have.text', '-5');
-      cy.mount(<Counter value={-3} setValue={setCounterValue} maximum={-7} />);
+
+      cy.mount(<CounterWrapper defaultValue={-3} maximum={-7} />);
       cy.get('.counter-value').should('have.text', '-7');
    });
 
 
    it('increments by 1 when increment button is pressed', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} />);
+      cy.mount(<CounterWrapper defaultValue={4} />);
       cy.get('#container > :nth-child(3)').click();
       cy.get('.counter-value').should('have.text', '5');
       cy.get('#container > :nth-child(3)').click();
@@ -73,7 +85,7 @@ describe('<Counter />', () => {
 
 
    it('decrements by 1 when decrement button is pressed', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} />);
+      cy.mount(<CounterWrapper defaultValue={4} />);
       cy.get('#container > :nth-child(1)').click();
       cy.get('.counter-value').should('have.text', '3');
       cy.get('#container > :nth-child(1)').click();
@@ -84,21 +96,21 @@ describe('<Counter />', () => {
 
 
    it('increments not above the user defined maximum', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} maximum={6} />);
+      cy.mount(<CounterWrapper defaultValue={5} maximum={6} />);
       cy.get('#container > :nth-child(3)').click();
       cy.get('.counter-value').should('have.text', '6');
       cy.get('#container > :nth-child(3)').click();
       cy.get('.counter-value').should('have.text', '6');
    });
 
+
    it('decrements not below the user defined minimum', () => {
-      cy.mount(<Counter value={counterValue} setValue={setCounterValue} minimum={3} />);
-      cy.get('#container > :nth-child(1)').click();
-      cy.get('.counter-value').should('have.text', '4');
+      cy.mount(<CounterWrapper defaultValue={4} minimum={3} />);
       cy.get('#container > :nth-child(1)').click();
       cy.get('.counter-value').should('have.text', '3');
       cy.get('#container > :nth-child(1)').click();
       cy.get('.counter-value').should('have.text', '3');
    });
+
 
 });
