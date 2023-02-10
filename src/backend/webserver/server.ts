281daa33eececd6ac1d20c77535d3abb9d3a728e
Lucas Vossen <lv_code@outlook.com>
2023-02-10 14:08:52 +0100

    implement route /api/Home/exirationDateOverview

--- src/backend/webserver/server.ts
+++ src/backend/webserver/server.ts
@@ -3,7 +3,7 @@ import { createPool, PoolConnection } from 'mariadb';
 import { createServer } from 'node:http';
 import * as dotenv from 'dotenv';
 import { handleSupplyOverviewHomeRequest } from './dataRequestHandler/GrocerySupplyOverviewHomeHandler';
-// import { handleExpirationDataRequest } from './dataRequestHandler/ExpirationDateRequestHandler';
+import { handleExpirationDateRequest } from './dataRequestHandler/ExpirationDateRequestHandler';
 
 
 dotenv.config();
@@ -11,33 +11,44 @@ dotenv.config();
 const app = express();
 const server = createServer(app);
 
-const dbConnectionPool = createPool({
-   host: process.env.DB_HOST,
-   port: parseInt(process.env.DB_PORT as string, 10),
-   user: process.env.DB_USER,
-   password: process.env.DB_PWD,
-   database: 'grocery_item_manager',
-   connectionLimit: 4
-});
+
+const dbConnectionPool = createPool(
+   {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string, 10),
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: 'grocery_item_manager',
+      connectionLimit: 4
+   }
+);
 
 
-async function handleRequest(
+type HandleRequestParam = {
    request: Request,
    response: Response,
-   handler: (request: Request, dbConnection: PoolConnection) => Promise<any>
-): Promise<void> {
+   handler: (
+      request: Request,
+      dbConnection: PoolConnection,
+      argumentList: string[]
+   ) => Promise<any>,
+   argumentList: string[]
+};
+
+
+async function handleRequest(param: HandleRequestParam): Promise<void> {
+   const { request, response, handler, argumentList } = param;
 
    let dbConnection: PoolConnection | undefined;
    try {
       dbConnection = await dbConnectionPool.getConnection();
-      const result = await handler(request, dbConnection);
-      await dbConnection.release();
+      const result = await handler(request, dbConnection, argumentList);
       response.json(result);
+      await dbConnection.release();
    } catch (error) {
       await dbConnection?.release();
       console.log(error);
    }
-
 }
 
 
@@ -45,15 +56,25 @@ app.use('/', express.static('../dist/public'));
 
 
 app.get('/api/Home/supplyOverview', async (request, response) => {
-   await handleRequest(request, response, handleSupplyOverviewHomeRequest);
+   const param = {
+      request,
+      response,
+      handler: handleSupplyOverviewHomeRequest,
+      argumentList: [] as string[]
+   };
+   await handleRequest(param);
 });
 
 
-/*
-app.get('/api/Home/expirationDateOverview/:dayLimit', (request, response) => {
-   response.json(handleExpirationDataRequest(request.params.dayLimit));
+app.get('/api/Home/expirationDateOverview/:dayLimit', async (request, response) => {
+   const param = {
+      request,
+      response,
+      handler: handleExpirationDateRequest,
+      argumentList: [request.params.dayLimit]
+   };
+   await handleRequest(param);
 });
-*/
 
 
 server.listen(8080);
