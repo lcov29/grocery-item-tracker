d4da4e7dd463900ecbfc47e8f7ecf9c0cb6a8ec0
Lucas Vossen <lv_code@outlook.com>
2023-02-12 22:58:27 +0100

    update styling

--- src/frontend/components/application-components/productDataAddDialog/ProductDataAddDialog.tsx
+++ src/frontend/components/application-components/productDataAddDialog/ProductDataAddDialog.tsx
@@ -9,16 +9,17 @@ function ProductDataAddDialog(): ReactElement {
       <>
          <h2>Add New Product</h2>
          <form id="product-data-add-form" onSubmit={() => {}}>
-            <label htmlFor="input-product-name">Product Name</label>
+            <label htmlFor="input-product-name">Product</label>
             <input type="text" id="input-product-name" />
             <label htmlFor="input-category-name">Category</label>
             <SearchableDropdown id="input-category-name" optionList={['Category 1', 'Category 2']} />
             <label htmlFor="input-subcategory-name">Subcategory</label>
             <SearchableDropdown id="input-subcategory-name" optionList={['Subcategory 1', 'Subcategory 2']} />
             <label htmlFor="input-weight">Weight</label>
-            <input type="number" id="input-weight" />
-            <label htmlFor="input-unit">Unit</label>
-            <SearchableDropdown id="input-unit" optionList={['g', 'L']} />
+            <div id="input-weight-container">
+               <input type="number" id="input-weight" />
+               <SearchableDropdown id="input-unit" optionList={['g', 'L']} />
+            </div>
             <div id="save-button-container">
                <button type="button" onClick={() => {}}>Save</button>
             </div>
