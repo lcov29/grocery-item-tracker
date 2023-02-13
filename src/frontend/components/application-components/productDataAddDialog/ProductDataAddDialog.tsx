625cd6e60fa59ee3e5e5d04d99832b3f9fa7ee90
Lucas Vossen <lv_code@outlook.com>
2023-02-13 12:15:13 +0100

    adjust styling

--- src/frontend/components/application-components/productDataAddDialog/ProductDataAddDialog.tsx
+++ src/frontend/components/application-components/productDataAddDialog/ProductDataAddDialog.tsx
@@ -9,21 +9,21 @@ function ProductDataAddDialog(): ReactElement {
       <>
          <h2>Add New Product</h2>
          <form id="product-data-add-form" onSubmit={() => {}}>
-            <label htmlFor="input-product-name">Product</label>
-            <input type="text" id="input-product-name" />
-            <label htmlFor="input-category-name">Category</label>
-            <SearchableDropdown id="input-category-name" optionList={['Category 1', 'Category 2']} />
-            <label htmlFor="input-subcategory-name">Subcategory</label>
+            <label htmlFor="input-product-data-add-product-name" className="product-data-label">Product</label>
+            <input type="text" id="input-product-data-add-product-name" />
+            <label htmlFor="input-product-data-category-name" className="product-data-label">Category</label>
+            <SearchableDropdown id="input-product-data-category-name" optionList={['Category 1', 'Category 2']} />
+            <label htmlFor="input-subcategory-name" className="product-data-label">Subcategory</label>
             <SearchableDropdown id="input-subcategory-name" optionList={['Subcategory 1', 'Subcategory 2']} />
-            <label htmlFor="input-weight">Weight</label>
+            <label htmlFor="input-weight" className="product-data-label">Weight</label>
             <div id="input-weight-container">
                <input type="number" id="input-weight" />
                <SearchableDropdown id="input-unit" optionList={['g', 'L']} />
             </div>
-            <div id="save-button-container">
-               <button type="button" onClick={() => {}}>Save</button>
-            </div>
          </form>
+         <div id="product-data-save-button-container">
+            <button type="button" onClick={() => {}}>Save</button>
+         </div>
       </>
    );
 }
