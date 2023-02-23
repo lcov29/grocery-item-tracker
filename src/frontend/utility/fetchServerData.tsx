ca188bac796d6b9a1560a8c48a90d84bfa2db114
Lucas Vossen <lv_code@outlook.com>
2023-02-23 22:32:14 +0100

    add method sendData()

--- src/frontend/utility/fetchServerData.tsx
+++ src/frontend/utility/fetchServerData.tsx
@@ -7,9 +7,21 @@ async function fetchData<T>(route: string, handler: (value: T) => void) {
 }
 
 
+async function sendData<T>(route: string, payload: T) {
+   const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+   };
+
+   const response = await fetch(route, options);
+   return response;
+}
+
+
 function getPageId(): string {
    return window.location.hash.replace('#', '');
 }
 
 
-export { fetchData, getPageId };
+export { fetchData, sendData, getPageId };
