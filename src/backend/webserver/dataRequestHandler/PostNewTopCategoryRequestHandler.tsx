45c5bb3afa8fbf2853876cbf089f4dc593a7ab2e
Lucas Vossen <lv_code@outlook.com>
2023-05-08 14:36:15 +0200

    change return value of post request to contain only http status code

--- src/backend/webserver/dataRequestHandler/PostNewTopCategoryRequestHandler.tsx
+++ src/backend/webserver/dataRequestHandler/PostNewTopCategoryRequestHandler.tsx
@@ -3,16 +3,19 @@ import { PoolConnection } from 'mariadb';
 
 
 async function handleNewTopCategoryPostRequest(request: Request, dbConnection: PoolConnection):
-Promise<void> {
+Promise<{ ok: number }> {
    const categoryName: string = request.body?.category;
+
    try {
-      const resultSet = await dbConnection.query(
-         'Insert into Categories (name) values (?)',
-         [categoryName]
-      );
+      const resultSet = await dbConnection.query('Insert into Categories (name) values (?)', [categoryName]);
+      const isInsertionSuccessful = resultSet.affectedRows === 1;
+      const statusCode = (isInsertionSuccessful) ? 200 : 500;
       console.log(resultSet);
+
+      return { ok: statusCode };
    } catch (error) {
       console.log(error);
+      return { ok: 500 };
    }
 }
 
