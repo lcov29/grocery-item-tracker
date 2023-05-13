2295368d98473a4c9a263b7594ea0c3dc0b0722d
Lucas Vossen <lv_code@outlook.com>
2023-05-13 23:23:03 +0200

    add file

---
+++ src/frontend/components/pages/grocerySupplyOverview/GrocerySupplyOverview.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement, useState } from 'react';
+import { SupplyOverview } from '../../application-components/supplyOverview/SupplyOverview';
+import { SupplyProductDetail } from '../../application-components/supplyProductDetail/SupplyProductDetail';
+import { ProductInSupplyDatabaseRecord } from '../../../../tsDataTypes/tsTypesGrocerySupplyOverviewHome';
+import './grocerySupplyOverview.css';
+
+
+function GrocerySupplyOverview(): ReactElement {
+   const [currentProduct, setCurrentProduct] = useState<ProductInSupplyDatabaseRecord>();
+
+   return (
+      <main id="grocery-supply-overview-container-main">
+         <div className="grocery-supply-overview-container">
+            <SupplyOverview />
+         </div>
+         <div className="grocery-supply-overview-container">
+            <SupplyProductDetail
+               currentProduct={currentProduct}
+               setCurrentProduct={setCurrentProduct}
+            />
+         </div>
+      </main>
+   );
+}
+
+
+export { GrocerySupplyOverview };
