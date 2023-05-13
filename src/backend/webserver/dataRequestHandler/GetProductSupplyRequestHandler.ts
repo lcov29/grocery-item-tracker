2125b7c2c4b97ad3c47032840f7ce6963b58ec09
Lucas Vossen <lv_code@outlook.com>
2023-05-14 01:28:18 +0200

    add file

---
+++ src/backend/webserver/dataRequestHandler/GetProductSupplyRequestHandler.ts
@@ -0,0 +1,21 @@
+import { Request } from 'express';
+import { PoolConnection } from 'mariadb';
+import { SupplyListDatabaseRecord } from '../../../tsDataTypes/tsTypesGrocerySupply';
+
+
+async function handleProductSupplyRequest(
+   request: Request,
+   dbConnection: PoolConnection,
+   argumentList: string[]
+): Promise<SupplyListDatabaseRecord[]> {
+
+   const productId = argumentList[0];
+   const resultSet: SupplyListDatabaseRecord[] = await dbConnection.query(
+      'select * from GrocerySupplyList where product = (select name from Products where id = ?);',
+      [productId]
+   );
+   return resultSet.slice();
+}
+
+
+export { handleProductSupplyRequest };
