e880ec007d9ec86abf0d7377ba85b49bd2de1e62
Lucas Vossen <lv_code@outlook.com>
2023-05-14 15:53:42 +0200

    add sorting to sql statement

--- src/backend/webserver/dataRequestHandler/GetProductSupplyRequestHandler.ts
+++ src/backend/webserver/dataRequestHandler/GetProductSupplyRequestHandler.ts
@@ -11,7 +11,10 @@ async function handleProductSupplyRequest(
 
    const productId = argumentList[0];
    const resultSet: SupplyListDatabaseRecord[] = await dbConnection.query(
-      'select * from GrocerySupplyList where product = (select name from Products where id = ?);',
+      `select * 
+      from GrocerySupplyList 
+      where product = (select name from Products where id = ?)
+      order by expirationDate asc;`,
       [productId]
    );
    return resultSet.slice();
