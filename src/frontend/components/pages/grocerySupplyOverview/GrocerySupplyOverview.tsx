cee8db7428faf3ebfdac09761eb44ec542793bfc
Lucas Vossen <lv_code@outlook.com>
2023-05-16 23:05:21 +0200

    add state from component SupplyOverview and add handler for clicking on table row

--- src/frontend/components/pages/grocerySupplyOverview/GrocerySupplyOverview.tsx
+++ src/frontend/components/pages/grocerySupplyOverview/GrocerySupplyOverview.tsx
@@ -1,20 +1,37 @@
-import React, { ReactElement, useState } from 'react';
+/* eslint-disable react/jsx-no-bind */
+import React, { ReactElement, useEffect, useState } from 'react';
 import { SupplyOverview } from '../../application-components/supplyOverview/SupplyOverview';
 import { SupplyProductDetail } from '../../application-components/supplyProductDetail/SupplyProductDetail';
 import { ProductListDatabaseRecord } from '../../../../tsDataTypes/tsTypesGrocerySupplyOverview';
+import { fetchData } from '../../../utility/fetchServerData';
 import './grocerySupplyOverview.css';
 
 
 function GrocerySupplyOverview(): ReactElement {
+   const [productList, setProductList] = useState<ProductListDatabaseRecord[]>([]);
    const [currentProduct, setCurrentProduct] = useState<ProductListDatabaseRecord>();
+
+
+   useEffect(() => {
+      const route = '/api/grocerySupplyOverview/productList';
+      fetchData<ProductListDatabaseRecord[]>(route, setProductList);
+   }, []);
+
+
+   function handleSupplyOverviewRowListClick(productName: string): void {
+      const newProduct = productList.find((product) => product.name === productName);
+      setCurrentProduct(newProduct);
+   }
+
 
    return (
       <main id="grocery-supply-overview-container-main">
          <div className="grocery-supply-overview-container">
-            <SupplyOverview />
+            <SupplyOverview handleRowClick={handleSupplyOverviewRowListClick} />
          </div>
          <div className="grocery-supply-overview-container">
             <SupplyProductDetail
+               productList={productList}
                currentProduct={currentProduct}
                setCurrentProduct={setCurrentProduct}
             />
