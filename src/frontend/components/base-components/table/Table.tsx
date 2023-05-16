4d3bf7fa6d22b53ccb68d562312611a6c9a7c785
Lucas Vossen <lv_code@outlook.com>
2023-05-16 22:58:50 +0200

    implement optional handler for clicking on table row

--- src/frontend/components/base-components/table/Table.tsx
+++ src/frontend/components/base-components/table/Table.tsx
@@ -4,47 +4,56 @@ import './table.css';
 
 type TableProps = {
    headerList: string[],
-   rowList: (string | ReactElement)[][]
+   rowList: (string | ReactElement)[][],
+   keyList?: string[],
+   handleRowClick?: (key: string) => void
 };
 
 
-function buildTableDataField(data: string | ReactElement, keyValue: number): ReactElement {
-   const isString = typeof data === 'string';
-   const className = (isString) ? '' : 'reactElementField';
-   return <td key={keyValue} className={className}>{data}</td>;
-}
-
-
-function buildTableRow(index: number, rowDataList: (string | ReactElement)[]): ReactElement {
-   return (
-      <tr key={index}>
-         {rowDataList.map((data, columnIndex) => buildTableDataField(data, columnIndex))}
-      </tr>
-   );
-}
-
-
-function buildTableBody(rowList: (string | ReactElement)[][]): ReactElement {
-   return <tbody>{rowList.map((rowData, index) => buildTableRow(index, rowData))}</tbody>;
-}
-
-
-function buildTableHeader(headerList: string[]): ReactElement {
-   return (
-      <thead>
-         <tr key="header">{headerList.map((header, index) => <th key={index}>{header}</th>)}</tr>
-      </thead>
-   );
-}
-
-
 function Table(props: TableProps): ReactElement {
-   const { headerList, rowList } = props;
+   const { headerList, rowList, keyList = [], handleRowClick = () => {} } = props;
+
+
+   function buildTableHeader(): ReactElement {
+      return (
+         <thead>
+            <tr key="header">{headerList.map((header, index) => <th key={index}>{header}</th>)}</tr>
+         </thead>
+      );
+   }
+
+
+   function handleTableRowClick(index: number): void {
+      const key = keyList[index];
+      handleRowClick(key);
+   }
+
+
+   function buildTableDataField(data: string | ReactElement, keyValue: number): ReactElement {
+      const isString = typeof data === 'string';
+      const className = (isString) ? '' : 'reactElementField';
+      return <td key={keyValue} className={className}>{data}</td>;
+   }
+
+
+   function buildTableRow(index: number, rowDataList: (string | ReactElement)[]): ReactElement {
+      return (
+         <tr key={index} onClick={() => { handleTableRowClick(index); }}>
+            {rowDataList.map((data, columnIndex) => buildTableDataField(data, columnIndex))}
+         </tr>
+      );
+   }
+
+
+   function buildTableBody(): ReactElement {
+      return <tbody>{rowList.map((rowData, index) => buildTableRow(index, rowData))}</tbody>;
+   }
+
 
    return (
       <table key="table" className="itemTable">
-         { buildTableHeader(headerList) }
-         { buildTableBody(rowList) }
+         { buildTableHeader() }
+         { buildTableBody() }
       </table>
    );
 }
