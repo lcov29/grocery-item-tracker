8a7a8a85def422860edd4a453b145b22d5b99956
Lucas Vossen <lv_code@outlook.com>
2023-05-16 23:01:33 +0200

    add support for handling click on table row

--- src/frontend/components/application-components/supplyOverview/SupplyOverview.tsx
+++ src/frontend/components/application-components/supplyOverview/SupplyOverview.tsx
@@ -11,58 +11,73 @@ import {
 import './supplyOverview.css';
 
 
-function generateProductTable(productList: ProductSupplyOverview[] | undefined):
-ReactElement[] | [] {
-   if (productList) {
-      const headerList = ['Product', 'Amount'];
-      const rowList: string[][] = [];
-      productList.forEach((product) => rowList.push([product.name, `${product.total}`]));
-      return [<Table headerList={headerList} rowList={rowList} key={1} />];
-   }
-   return [];
-}
+type Props = {
+   handleRowClick?: (key: string) => void
+};
 
 
-function generateSubCategory(subCategory: SubCategory, key: number): ReactElement {
-   return (
-      <Category
-         name={subCategory.name}
-         additionalText={`${subCategory.total} Items`}
-         contentList={generateProductTable(subCategory.productList)}
-         isContentLevel
-         key={key}
-      />
-   );
-}
-
-
-function generateTopCategory(topCategory: TopCategory, key: number): ReactElement {
-   let contentList: ReactElement[] | [] = [];
-   if (topCategory.subCategoryList) {
-      contentList = topCategory.subCategoryList.map(
-         (subcategory, index) => generateSubCategory(subcategory, index)
-      );
-   }
-
-   return (
-      <Category
-         name={topCategory.name}
-         additionalText={`${topCategory.total} Items`}
-         contentList={contentList}
-         isTopLevel
-         key={key}
-      />
-   );
-}
-
-
-function SupplyOverview(): ReactElement | null {
+function SupplyOverview(props: Props): ReactElement | null {
+   const { handleRowClick = () => {} } = props;
    const [supplyOverviewData, setSupplyOverviewData] = useState<SupplyOverviewFrontendData>();
 
 
    useEffect(() => {
       fetchData<SupplyOverviewFrontendData>('/api/supplyOverview', setSupplyOverviewData);
    }, []);
+
+
+   function generateProductTable(productList: ProductSupplyOverview[] | undefined):
+   ReactElement[] | [] {
+      if (productList) {
+         const headerList = ['Product', 'Amount'];
+         const rowList = productList.map((product) => [product.name, `${product.total}`]);
+         const keyList = productList.map((product) => product.name);
+
+         return [
+            <Table
+               headerList={headerList}
+               rowList={rowList}
+               keyList={keyList}
+               key={1}
+               handleRowClick={handleRowClick}
+            />
+         ];
+      }
+      return [];
+   }
+
+
+   function generateSubCategory(subCategory: SubCategory, key: number): ReactElement {
+      return (
+         <Category
+            name={subCategory.name}
+            additionalText={`${subCategory.total} Items`}
+            contentList={generateProductTable(subCategory.productList)}
+            isContentLevel
+            key={key}
+         />
+      );
+   }
+
+
+   function generateTopCategory(topCategory: TopCategory, key: number): ReactElement {
+      let contentList: ReactElement[] | [] = [];
+      if (topCategory.subCategoryList) {
+         contentList = topCategory.subCategoryList.map(
+            (subcategory, index) => generateSubCategory(subcategory, index)
+         );
+      }
+
+      return (
+         <Category
+            name={topCategory.name}
+            additionalText={`${topCategory.total} Items`}
+            contentList={contentList}
+            isTopLevel
+            key={key}
+         />
+      );
+   }
 
 
    function generateGrocerySupplyOverview(): ReactElement | null {
