ce26e39b82324f8da21d0d4b3728584da6896f80
Lucas Vossen <lv_code@outlook.com>
2023-05-24 01:04:48 +0200

    update css selectors

--- src/frontend/components/pages/supplyOverview/subComponents/productSupply/ProductSupply.tsx
+++ src/frontend/components/pages/supplyOverview/subComponents/productSupply/ProductSupply.tsx
@@ -53,7 +53,7 @@ function ProductSupply(props: Props): ReactElement {
       if (isRenderable()) {
          const product = currentProductData[0];
          return (
-            <p id="supply-product-detail-breadcrumb">
+            <p className="product-supply-breadcrumb">
                {`${product.topcategory} > ${product.subcategory} > `}
                <b>{product.product}</b>
             </p>
@@ -93,7 +93,7 @@ function ProductSupply(props: Props): ReactElement {
 
    return (
       <>
-         <div id="supply-product-detail-header">
+         <div className="product-supply-header">
             <h2>Product Supply</h2>
             <SearchBar
                id="supply-product-detail-searchbar"
