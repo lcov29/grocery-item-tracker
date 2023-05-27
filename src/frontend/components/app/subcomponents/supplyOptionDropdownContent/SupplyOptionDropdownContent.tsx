9e592b6de410a501d46ce8823ce568c9a5acfbf9
Lucas Vossen <lv_code@outlook.com>
2023-05-27 23:14:06 +0200

    replace option button text with icons

--- src/frontend/components/app/subcomponents/supplyOptionDropdownContent/SupplyOptionDropdownContent.tsx
+++ src/frontend/components/app/subcomponents/supplyOptionDropdownContent/SupplyOptionDropdownContent.tsx
@@ -1,5 +1,9 @@
 import React, { ReactElement } from 'react';
 import { pageId } from '../../../../routing/routing';
+import { NavigationIcon } from '../NavigationIcon';
+import supplyOverviewIcon from '../../../../icons/supplyIcon.svg';
+import itemAddIcon from '../../../../icons/itemAddIcon.svg';
+import itemRemoveIcon from '../../../../icons/itemRemoveIcon.svg';
 import './supplyOptionDropdownContent.css';
 
 
@@ -14,20 +18,29 @@ function SupplyOptionDropdownContent(props: Props): ReactElement {
       <div className="menu-grocery-dropdown-content">
          <div className="menu-grocery-dropdown-content-title">Supply</div>
          <div className="menu-grocery-dropdown-content-suboptions">
-            <button type="button" onClick={() => { setCurrentPageId(pageId.supplyOverview); }}>
-               Overview
-            </button>
-            <button type="button" onClick={() => { setCurrentPageId(pageId.supplyMinimum); }}>
-               Minimum
+            <button
+               type="button"
+               title="Supply Details"
+               onClick={() => { setCurrentPageId(pageId.supplyOverview); }}
+            >
+               <NavigationIcon icon={supplyOverviewIcon} alternativeText="Current Supply" />
             </button>
          </div>
          <div className="menu-grocery-dropdown-content-title last-title">Grocery Items</div>
          <div className="menu-grocery-dropdown-content-suboptions">
-            <button type="button" onClick={() => { setCurrentPageId(pageId.addSupplyItems); }}>
-               Add
+            <button
+               type="button"
+               title="Add Items"
+               onClick={() => { setCurrentPageId(pageId.addSupplyItems); }}
+            >
+               <NavigationIcon icon={itemAddIcon} alternativeText="Add Items" />
             </button>
-            <button type="button" onClick={() => { setCurrentPageId(pageId.consumeSupplyItems); }}>
-               Consume
+            <button
+               type="button"
+               title="Consume Items"
+               onClick={() => { setCurrentPageId(pageId.consumeSupplyItems); }}
+            >
+               <NavigationIcon icon={itemRemoveIcon} alternativeText="Consume Items" />
             </button>
          </div>
       </div>
