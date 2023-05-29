7b961404078807fc11d02c825642d152d12a6780
Lucas Vossen <lv_code@outlook.com>
2023-05-29 21:52:00 +0200

    add test cases for function parsePriceInput()

--- src/frontend/utility/currencyFunctions/currencyFunctions.test.ts
+++ src/frontend/utility/currencyFunctions/currencyFunctions.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { describe, test, expect } from '@jest/globals';
-import { parseDatabasePrice } from './currencyFunctions';
+import { parseDatabasePrice, parsePriceInput } from './currencyFunctions';
 
 
 describe('currencyFunctions', () => {
@@ -18,6 +18,23 @@ describe('currencyFunctions', () => {
          expect(parseDatabasePrice(100)).toBe('1,00 €');
          expect(parseDatabasePrice(100000)).toBe('1.000,00 €');
          expect(parseDatabasePrice(100000000)).toBe('1.000.000,00 €');
+      });
+
+   });
+
+
+   describe('parsePriceInput()', () => {
+
+      test('parses string input containing numbers correctly', () => {
+         expect(parsePriceInput('0,24 €')).toBe(24);
+         expect(parsePriceInput('1,24 €')).toBe(124);
+         expect(parsePriceInput('2.564.345,24 €')).toBe(256434524);
+      });
+
+      test('returns NaN for strings without any number', () => {
+         expect(parsePriceInput('')).toBe(NaN);
+         expect(parsePriceInput(' ')).toBe(NaN);
+         expect(parsePriceInput('foo bar %&/')).toBe(NaN);
       });
 
    });
