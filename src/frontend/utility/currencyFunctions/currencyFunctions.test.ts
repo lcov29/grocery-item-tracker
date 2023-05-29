4af0d962cfeead0ff80cc5c1d89b70db1a97abb8
Lucas Vossen <lv_code@outlook.com>
2023-05-29 21:01:17 +0200

    update import of renamed function

--- src/frontend/utility/currencyFunctions/currencyFunctions.test.ts
+++ src/frontend/utility/currencyFunctions/currencyFunctions.test.ts
@@ -1,23 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { describe, test, expect } from '@jest/globals';
-import { formatDatabasePrice } from './currencyFunctions';
+import { parseDatabasePrice } from './currencyFunctions';
 
 
 describe('currencyFunctions', () => {
 
 
-   describe('formatDatabasePrice()', () => {
+   describe('parseDatabasePrice()', () => {
 
       test('formats price with fractional part correctly', () => {
-         expect(formatDatabasePrice(1234)).toBe('12,34 €');
-         expect(formatDatabasePrice(123456)).toBe('1.234,56 €');
-         expect(formatDatabasePrice(123456789)).toBe('1.234.567,89 €');
+         expect(parseDatabasePrice(1234)).toBe('12,34 €');
+         expect(parseDatabasePrice(123456)).toBe('1.234,56 €');
+         expect(parseDatabasePrice(123456789)).toBe('1.234.567,89 €');
       });
 
       test('pads price without fractional part correctly', () => {
-         expect(formatDatabasePrice(100)).toBe('1,00 €');
-         expect(formatDatabasePrice(100000)).toBe('1.000,00 €');
-         expect(formatDatabasePrice(100000000)).toBe('1.000.000,00 €');
+         expect(parseDatabasePrice(100)).toBe('1,00 €');
+         expect(parseDatabasePrice(100000)).toBe('1.000,00 €');
+         expect(parseDatabasePrice(100000000)).toBe('1.000.000,00 €');
       });
 
    });
