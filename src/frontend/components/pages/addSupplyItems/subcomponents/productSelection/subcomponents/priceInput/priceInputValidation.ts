e57d63aaa599deace01254178af8b35e180f67e1
Lucas Vossen <lv_code@outlook.com>
2023-05-29 13:10:16 +0200

    add file

---
+++ src/frontend/components/pages/addSupplyItems/subcomponents/productSelection/subcomponents/priceInput/priceInputValidation.ts
@@ -0,0 +1,57 @@
+function isInputEmpty(input: string): boolean {
+   return input === '';
+}
+
+
+function isContainingOnlyValidCharacters(input: string): boolean {
+   return input.search(/[^0-9.,]+/g) === -1;
+}
+
+
+function isContainingNoLeadingZero(input: string): boolean {
+   return !input.startsWith('0');
+}
+
+
+function isContainingAtMostOneDecimalSeparator(input: string): boolean {
+   const separatorList = input.match(/[.,]/g);
+   if (separatorList) {
+      return separatorList.length === 1;
+   }
+   return true;
+}
+
+
+function isAmountOfCharactersAfterDecimalSeparatorCorrect(input: string): boolean {
+   const hasDecimalSeparator = input.match(/[.,]/g) !== null;
+
+   if (hasDecimalSeparator) {
+      const decimalSeparatorIndex = input.search(/[.,]/g);
+      const stringAfterSeparator = input.substring(decimalSeparatorIndex + 1);
+      return [1, 2].includes(stringAfterSeparator.length);
+   }
+   return false;
+}
+
+
+function isPriceInputValid(input: string): boolean {
+   const price = input.replaceAll(' ', '');
+   return (
+      isInputEmpty(price) || (
+         isContainingOnlyValidCharacters(price)
+         && isContainingNoLeadingZero(price)
+         && isContainingAtMostOneDecimalSeparator(price)
+         && isAmountOfCharactersAfterDecimalSeparatorCorrect(price)
+      )
+   );
+}
+
+
+export {
+   isInputEmpty,
+   isContainingOnlyValidCharacters,
+   isContainingNoLeadingZero,
+   isContainingAtMostOneDecimalSeparator,
+   isAmountOfCharactersAfterDecimalSeparatorCorrect,
+   isPriceInputValid
+};
